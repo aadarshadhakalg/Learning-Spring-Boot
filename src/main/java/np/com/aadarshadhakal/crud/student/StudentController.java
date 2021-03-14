f169a319870aa72f946d5089bb62f68d0aab51ce
aadarshadhakalg <braveboy2058@gmail.com>
2021-03-14 08:55:11 +0545

    done

--- src/main/java/np/com/aadarshadhakal/crud/student/StudentController.java
+++ src/main/java/np/com/aadarshadhakal/crud/student/StudentController.java
@@ -3,8 +3,14 @@ package np.com.aadarshadhakal.crud.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +28,20 @@ public class StudentController {
 	@GetMapping
 	public List<Student> getStudents() {
 		return studentService.getStudents();
+	}
+	
+	@PostMapping
+	public void registerNewStudent(@RequestBody Student student) {
+		studentService.addNewSudent(student);
+	}
+	
+	@DeleteMapping(path = "{studentId}")
+	public void deleteStudent(@PathVariable("studentId") Long id) {
+		studentService.deleteStudent(id);
+	}
+	
+	@PutMapping(path = "{studentId}")
+	public void updateStudent(@PathVariable("studentId") Long id,@RequestParam(required = false) String name,@RequestParam(required = false) String email) {
+		studentService.updateStudent(id,name, email);
 	}
 }
--- src/main/java/np/com/aadarshadhakal/crud/student/StudentService.java
+++ src/main/java/np/com/aadarshadhakal/crud/student/StudentService.java
@@ -1,6 +1,9 @@
 package np.com.aadarshadhakal.crud.student;
 
 import java.util.List;
+import java.util.Objects;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +21,42 @@ public class StudentService {
 	
 	public List<Student> getStudents() {
 		return studentRepository.findAll();
+	}
+	
+	public void addNewSudent(Student student) {
+		if(studentRepository.findStudentByEmail(student.getEmail()).isPresent() ) {
+			throw new IllegalStateException("Student with this email already exists.");
+		}
+		studentRepository.save(student);
+	}
+
+
+	public void deleteStudent(Long id) {
+		if(studentRepository.existsById(id)) {
+			studentRepository.deleteById(id);
+		}else {
+			throw new IllegalStateException("Student with id" + id + "doesnot exists");
+		}
+		
+	}
+
+	@Transactional
+	public void updateStudent(Long id, String name, String email) {
+		Student student = studentRepository.findById(id).orElseThrow(()->  new IllegalStateException("Student with id" + id + "doesnot exists") );
+		
+		if(name !=null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		
+		if(email !=null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+			
+			if(studentRepository.findStudentByEmail(email).isPresent()) {
+				throw new IllegalStateException("Email Already Taken");
+			}else {
+
+				student.setEmail(email);
+			}
+		}
+		
 	}
 }
